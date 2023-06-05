200468d92537288a9374d4fd44d1823a1ab02806
ankit kumar maurya <mauryaankit482@gmail.com>
2023-06-05 21:21:44 +0530

    dev - project init

---
+++ src/main/java/com/retail/kynaara/KynaaraApplication.java
@@ -0,0 +1,13 @@
+package com.retail.kynaara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KynaaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KynaaraApplication.class, args);
+	}
+
+}
